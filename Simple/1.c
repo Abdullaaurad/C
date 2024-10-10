377f14f4c22f25537be71afddc82e443b888379a
abdullaaurad <abdullaaurad@gmail.com>
2024-10-10 20:20:32 +0300

    edited

--- Simple/1.c
+++ Simple/1.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
-int power(int a, int b) {
+// Function to calculate the sum of squares of integers from a to b
+int sumOfSquares(int a, int b) {
     int sum = 0;
     for (int i = a; i <= b; i++) {
-        sum = sum + (i * i);
+        sum += (i * i);  // Sum of squares
     }
     return sum;
 }
 
 int main() {
-    int a;
-    int b;
-    int sum;
-    printf("Enter integers for a and b: ");
+    int a, b, sum;
+
+    // Input
+    printf("Enter integers for a and b (a <= b): ");
     scanf("%d %d", &a, &b);
-    sum = power(a, b);
-    printf("The sum is: %d\n", sum);
+
+    // Validate that a <= b
+    if (a > b) {
+        printf("Error: a should be less than or equal to b.\n");
+        return 1;  // Exit with error code
+    }
+
+    // Calculate sum of squares
+    sum = sumOfSquares(a, b);
+
+    // Output result
+    printf("The sum of squares from %d to %d is: %d\n", a, b, sum);
 
     return 0;
 }
